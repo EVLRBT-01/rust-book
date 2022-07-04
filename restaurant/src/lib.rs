979ee2f813b24cfc15630b24d876ec330971f6f9
Frank Bell <fb@evilrobot.net>
2022-07-04 01:15:29 +0100

    Modules

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,14 @@
+mod back_of_house;
+mod customer;
+mod front_of_house;
+
+fn deliver_order() {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
